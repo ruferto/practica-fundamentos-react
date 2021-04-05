dbb414d71d9307747ea7bba68b6e1f4223412bdb
Andrés <usuario616@gmail.com>
2021-04-05 10:13:20 +0200

    Component for advert items

--- src/components/AdsList.js
+++ src/components/AdsList.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import Tags from './Tags';
 import { AuthContextConsumer } from '../components/auth/context';
 import QueryForm from './QueryForm';
-import { Link } from 'react-router-dom';
+import Advert from './Advert';
 import { getLatestAdverts } from '../api/adverts';
 import storage from '../utils/storage';
 
@@ -68,28 +67,20 @@ const AdsList = ({ queries, setQueries, me }) => {
   );
 
   const adsElement = filtered.map( ad => {
-    
-    return <li className='ad-container' key={ad.id}>
-      <Link to={{pathname: `/advert/${ad.id}`, queries:{queries}}} >
-        <div>
-        { (ad.sale) ? 'Sell' : 'Buy' }:
-        <div className='ad-name'><b>{ ad.name }</b></div>
-        <div className='price'>{ ad.price % 1 !== 0 ? Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(ad.price) : Intl.NumberFormat('de-DE').format(ad.price)+' €' }</div>
-        <div className='tags'>Etiquetas: &nbsp;<Tags tagsArray={ad.tags}/></div>
-        </div>
-        </Link>
-      </li>
+    return <Advert ad={ad} queries={queries} key={ad.id}/>
   });
 
-    if(isLoading) return <div className='lds-roller'><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-    return (
-      <>
+    if(isLoading)
+      return <div className='lds-roller'><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>;
+
+    return <>
         <QueryForm queries={queries} setQueries={setQueries} handleChange={handleChange} handleReset={handleReset}/>
         <div className='ads-list'>
-          {adsElement.length !==0 ? adsElement : <div style={{fontSize: 20, paddingTop:40, textAlign:'center'}}>No hay anuncios con ese filtro<br /><a href='/login'>Prueba a crear uno</a></div>}{/*<Redirect to={`/login`} />}*/}
+          {adsElement.length !==0 ? 
+          adsElement : 
+          <div style={{fontSize: 20, paddingTop:40, textAlign:'center'}}>No hay anuncios con ese filtro<br /><a href='/login'>Prueba a crear uno</a></div>}
         </div>
-      </>
-    );
+      </>;
   }
 
   const ConnectedAdsList = props => {
