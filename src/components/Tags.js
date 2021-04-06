22641962e69e330ecf072e2d35f760c78a1dc6ef
Andrés <usuario616@gmail.com>
2021-04-06 16:04:07 +0200

    Refactor tags

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -2,11 +2,7 @@
 const Tags = ( {tagsArray} ) => {
     let tags = '';
     for(let i=0; i<=tagsArray.length-1; i++){
-        if(i === tagsArray.length-1){
-            tags += `${tagsArray[i]}`;
-        }else{
-            tags += `${tagsArray[i]}, `
-        }
+        tags += (i === tagsArray.length-1) ? `${tagsArray[i]}` : `${tagsArray[i]}, `;
     }
     
     return <div>{tags}</div>
