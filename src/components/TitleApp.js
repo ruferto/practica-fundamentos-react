1af1c3343788b713a94b56627952bddc0097b03e
Andrés <usuario616@gmail.com>
2021-04-10 16:48:45 +0200

    Added NavLinks

--- src/components/TitleApp.js
+++ src/components/TitleApp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { logout } from '../api/auth';
 import { AuthContextConsumer } from './auth/context';
 import ConfirmationPanel from './ConfirmationPanel';
@@ -24,10 +24,10 @@ const TitleApp = ({isLogged, onLogout, me}) => {
     return <div className='title-container'>
     {TryingToLogout ? 
     <ConfirmationPanel deleteSure={handleLogout} cancelDelete={cancelLogout} message={'Do you really want to leave?'} subtitle={''} /> : ''}
-    <h1><div className='title' ><Link to='/' className='title' >NodePop</Link></div></h1>
+    <h1><div className='title' ><NavLink to='/adverts' className='title' activeStyle={{textShadow: '0px 3px 2px black'}} >NodePop</NavLink></div></h1>
     <div className='subtitle-container'>
       {isLogged ? <>{ me ? <div style={{textAlign: 'center', paddingBottom:5}}>Logged as <b>{me}</b></div> : ''} <button style={{marginBottom:30, fontSize:15}} onClick={preLogout}><img src='/images/logout.png' width='14' alt='logout'/> Logout</button> 
-      <div><Link to='/advert/new'><button id='myBtn' className='buttonAdd' ><b>+</b> New Advert</button></ Link></div></>
+      <div><NavLink to='/advert/new' className='newAdLink' activeClassName='activeLink' ><b>+</b> New Advert</ NavLink></div></>
       : ''}
     </div>
   </div>;
